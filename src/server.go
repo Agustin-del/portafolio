3dd818c78fe1d11fe39c11f7e9b1892151035149
Agustin-del <eddyruveda@gmail.com>
2026-01-26 20:04:32 -0300

    prueba

--- src/server.go
+++ src/server.go
@@ -9,6 +9,27 @@ import (
 	"portafolio/ui/pages"
 )
 
+type Proyecto struct{
+	Titulo string
+	Descripcion_general string
+	Descripcion_detallada string
+	fuentes []string
+}
+
+type Contacto struct {
+	Email string
+	Subject string
+	Message string
+}
+
+func noCache(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set("Cache-Control", "no-store", "no-cache", "must-revalidate")
+		c.Response().Header().Set("Pragma", "no-cache")
+		c.Response().Header().Set("Expires", "0")
+		return next(c)
+	}
+}
 func isHtmx(c echo.Context) bool {
 	return c.Request().Header.Get("Hx-Request") == "true"
 }
@@ -23,14 +44,24 @@ func render(c echo.Context, component templ.Component) error {
 	return nil
 }
 
-func main() {
-	e := echo.New()
+func newContacto (email, subject, message string) Contacto{
+	return Contacto{
+		Email: email,
+		Subject: subject,
+		Message: message,
+	}
+}
 
-	e.Static("/imagenes", "static/imagenes")
-	e.Static("/estilos", "static/estilos")
+func main() {
+	//var proyecto []Proyecto
+
+	e := echo.New()
 
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
+
+	e.Static("/imagenes", "static/imagenes")
+	e.Static("/estilos", "static/estilos")
 
 	e.GET("/", func(c echo.Context) error {
 		if isHtmx(c) {
@@ -41,6 +72,15 @@ func main() {
 	})
 
 	e.GET("/contacto", func(c echo.Context) error {
+		//TODO:validar datos	
+		/*
+		email := c.FormValue("email")
+		subject := c.FormValue("subject")
+		message := c.FormValue("message")
+		contacto := newContacto(email, subject, message)
+
+		*/
+		//TODO: enviarlo a gmail
 		if isHtmx(c) {
 			return render(c, pages.ContactoContenido())
 		}
