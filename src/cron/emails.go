e7c24c9993d8096bfb6e3645604c01f2841feb45
Agustin-del <eddyruveda@gmail.com>
2026-04-19 06:08:15 -0300

    anotaciones

--- src/cron/emails.go
+++ src/cron/emails.go
@@ -92,7 +92,14 @@ func IniciarCron() {
 	ticker := time.NewTicker(IntervaloMinutos * time.Minute)
 
 	log.Printf("Cron de emails iniciado (cada %d minutos)", IntervaloMinutos)
-
+//acaso esta linea es necesaria? Es decir no deberia a esperar a que el ticker me mande un tick para procesar los pendientes?
+//en un punto tiene sentido que procese todo cuando inicia y desde ahi cada vez que se cumple el tick, supongo
+//podria chequear con una peticion a la db si hay emails nuevos o pendendientes, y sino retornar de aca, es decir cerrar el ticker
+//digo para no estar corriendo siempre este cron durante todo el proceso, tendria que hacer algo en main para que cuando esa busqueda en la db
+//un select count(*) from mensajes where estado=pendiente sea mayor a que se yo x corra de vuelta este cron, proceso de a 10 mensajes
+//me parece poco sobre todo porque pasa concurrentemente. Acaso no seria mejor poner un minimo? Y este cron capaz dejarlo que corra,
+//pero pasar la logica a la funcion que procesa para que retorne si no hay suficientes mensajes... mm no la cosa no, porque quiero que me 
+//lleguen los mails. Igual el maximo me parece poco.
 	procesarPendientes(emisor)
 
 	for range ticker.C {
